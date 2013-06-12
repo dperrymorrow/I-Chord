26c071b9ded442ae572a88c904bbc74a5197cd36
David Morrow <dperrymorrow@gmail.com>
2013-06-12 10:19:26 -0700

    adding placeholder text to search field

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
   <script src="Chords.js" type="text/javascript" charset="utf-8"></script>
   <link rel="stylesheet" href="styles.css" type="text/css" media="screen" title="no title" charset="utf-8">
-  
+
   <meta name="apple-mobile-web-app-capable" content="yes" />
   <meta name="apple-mobile-web-app-status-bar-style" content="black"  />
   <meta name="viewport" content = "width = device-width, user-scalable = no"   />
@@ -13,11 +13,11 @@
 </head>
 <body>
   <div id="search">
-    <input type="text" name="chord" value="" id="chord">
+    <input type="text" name="chord" value="" placeholder="Begin typing a chord..." id="chord">
   </div>
-  
+
   <div id="results">
-    
+
   </div>
 </body>
 </html>
